ff517597f48357eac6caea11c4fce6fec66fa606
Yerin <dev.yerin@gmail.com>
2024-07-02 02:51:03 +0900

    feat: 리스트 구현

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -2,6 +2,8 @@
 
 import { getPosts } from "@/api/pokemon";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
 
 const HomePage = () => {
   const { data, isPending } = useQuery({
@@ -11,19 +13,30 @@ const HomePage = () => {
 
   if (isPending) return null;
 
-  console.log("@@", data);
-
   return (
     <main>
-      <div>하이</div>
-      {data.map((pokemon: any) => {
-        return (
-          <div key={pokemon.id}>
-            {pokemon.korean_name}
-            <img src={pokemon.sprites.front_default} alt="" />
-          </div>
-        );
-      })}
+      <h1 className="mb-4 text-center text-2xl font-bold">포켓몬 도감</h1>
+      <ul className="grid grid-cols-4 gap-4">
+        {data.map((pokemon: any) => {
+          return (
+            <li key={pokemon.id}>
+              <Link
+                href={`/detail/${pokemon.id}`}
+                className="flex flex-col items-center justify-center gap-2 rounded-md border p-4 shadow-md"
+              >
+                <strong>{pokemon.korean_name}</strong>
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+                <span>도감번호: {pokemon.id}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 };
