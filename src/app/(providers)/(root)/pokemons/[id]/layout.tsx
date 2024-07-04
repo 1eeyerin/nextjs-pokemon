27eb5aedf4915e05d9e1bc9214df2b81822c311d
Yerin <dev.yerin@gmail.com>
2024-07-05 04:34:59 +0900

    style: 너비 ... 수정

--- src/app/(providers)/(root)/pokemons/[id]/layout.tsx
+++ src/app/(providers)/(root)/pokemons/[id]/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 const DetailPageLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-50 to-indigo-100 py-10 h-dvh">
-      <div className="bg-white shadow-2xl rounded-3xl p-10 max-w-lg h-full overflow-y-auto">
+      <div className="bg-white shadow-2xl rounded-3xl p-10  h-full overflow-y-auto w-[500px]">
         {children}
       </div>
     </div>
